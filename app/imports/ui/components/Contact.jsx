62cc33c32bf96e613ccce725da0a8c5d0de9101a
jmnakano <jmnakano@hawaii.edu>
2023-03-30 01:34:47 -1000

    contact forms finished in 12:30 minutes.

--- app/imports/ui/components/Contact.jsx
+++ app/imports/ui/components/Contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Card, Image } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 const Contact = ({ contact }) => (
@@ -12,6 +13,7 @@ const Contact = ({ contact }) => (
     </Card.Header>
     <Card.Body>
       <Card.Text>{contact.description}</Card.Text>
+      <Link to={`/edit/${contact._id}`}>Edit</Link>
     </Card.Body>
   </Card>
 );
@@ -23,6 +25,7 @@ Contact.propTypes = {
     lastName: PropTypes.string,
     address: PropTypes.string,
     image: PropTypes.string,
+    _id: PropTypes.string,
     description: PropTypes.string,
   }).isRequired,
 };
